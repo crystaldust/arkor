a73b35f6281e7a51bbb9d52b65c0d7afdc3a5f8b
Zhen Ju <juzhen@huawei.com>
2016-08-22 15:09:43 +0800

    Add a ObjectID field to Fragment model. So the ORM association can be applied.

--- models/object.go
+++ models/object.go
@@ -10,15 +10,16 @@ import (
 type ObjectMeta struct {
 	gorm.Model
 	Content
-	ObjectID  string     `json:"object_id,omitempty" gorm:"column:object_id"`
+	ObjectID  string     `json:"object_id,omitempty" gorm:"unique"`
 	Key       string     `json:"object_key,omitempty" gorm:"column:object_key"`
 	Md5Key    string     `json:"md5_key,omitempty" gorm:"column:md5_key`
-	Fragments []Fragment `json:"fragments,omitempty" gorm:"ForeignKey:ObjectMetaID"`
+	Fragments []Fragment `json:"fragments,omitempty" gorm:"ForeignKey:ObjectID"`
 }
 
 type Fragment struct {
 	gorm.Model   `json:"-"`
-	ObjectMetaID string    `json:"-"`
+	ObjectID     uint32    `json:"-"` // Just for association
+	ObjectMetaID string    `json:"object_meta_id"`
 	FragmentID   string    `json:"id" gorm:"column:fragment_id"`
 	Index        int       `json:"index" gorm:"column:index"`
 	Start        int64     `json:"start"`
